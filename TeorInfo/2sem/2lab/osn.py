2871c954932c305fbd12bd4bd29d63b696572d93
doner <donierov2001@mail.ru>
2022-04-05 21:44:27 +0300

    вторая кнопка, ввод сумматоров, ошибка

--- TeorInfo/2sem/2lab/osn.py
+++ TeorInfo/2sem/2lab/osn.py
@@ -38,6 +38,45 @@ class mywindow(QtWidgets.QMainWindow):
                         sys.exit()
 
 
+    def summatori(self):
+        try:
+            global kolvo
+            global razrad
+            global sum
+
+            # Количество сумматоров
+            kolvo = int(self.ui.txt1.text())
+            razrad = [0,0,0]
+            spisick =[]
+            pomosh = True
+            #Ввод сумматоров
+            for i in range(kolvo):
+                spisi, ok = QInputDialog.getText(self, 'Input Dialog',
+                                         'Введите индексы через запятую:')
+                spisi2 = spisi.split(',')
+                for j in range(len(spisi2)):
+                    spisi2[j] = int(spisi2[j])
+                    if spisi2[j] > 3:
+                        pomosh = False
+                if pomosh == False:
+                    msg = QtWidgets.QMessageBox()
+                    msg.setWindowTitle('Ошибка!')
+                    msg.setText('Индекс вышел за рамки')
+                    msg.setIcon(msg.Warning)
+                    msg.exec()
+                    break
+
+                spisick.append(spisi2)
+
+                # Сумматоры в списке списков
+                sum = numpy.array(spisick)
+                print("Сумматоры:\n", sum)
+        except:
+            msg = QtWidgets.QMessageBox()
+            msg.setWindowTitle('Ошибка!')
+            msg.setText('Поля заполнены неправильно!')
+            msg.setIcon(msg.Warning)
+            msg.exec()
 
 
 

9f1cb980fdcfa1cb7d3cd85bb80a0730326a9eb9
doner <donierov2001@mail.ru>
2022-05-17 17:48:19 +0300

    прогон матрицы и вывод картинки

--- TeorInfo/3lab/osn.py
+++ TeorInfo/3lab/osn.py
@@ -20,3 +20,15 @@ numpydata = asarray(img)
 draw = ImageDraw.Draw(img)
 width = img.size[0]
 height = img.size[1]
+
+
+for i in range(height):
+    for j in range(width):
+        popa = []
+        for l in range(3):
+            a = bin(numpydata[i][j][l])[2:].zfill(8)
+            c = ''.join(oshibki(list(a)))
+            popa.append(int(c,2))
+        draw.point((j,i),(popa[0],popa[1],popa[2]))
+
+img.show()

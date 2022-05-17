36d0f266c08bfb63b6e0423ade4ec22734a563f2
doner <donierov2001@mail.ru>
2022-05-17 17:47:41 +0300

    внесение ошибок

--- TeorInfo/3lab/osn.py
+++ TeorInfo/3lab/osn.py
@@ -3,6 +3,16 @@ from numpy import asarray
 import random
 popa = []
 
+def oshibki(a):
+    spisok = [0, 1, 2, 3, 4, 5, 6, 7]
+    ind = random.sample(spisok, 2)
+    for i in range(len(ind)):
+        if int(a[ind[i]]) == 0:
+            a[ind[i]] = "1"
+        else:
+            a[ind[i]] = '0'
+    return a
+
 
 img = Image.open('two.jpg')
 numpydata = asarray(img)
